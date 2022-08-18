67e795c4597f537d901b85491b1c279005157e55
NikitaPK <nikitka.patrakov@mail.ru>
2022-08-18 16:12:08 +0300

    Title,Genres,Categories

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,9 +1,13 @@
 from django.urls import include, path
 from rest_framework.routers import SimpleRouter
 
-from reviews.views import CategoryViewSet, GenreViewSet, TitleViewSet
+from .views import CategoryViewSet, GenreViewSet, TitleViewSet
 
 router = SimpleRouter()
 router.register(r'categories', CategoryViewSet, basename='categories')
 router.register(r'genres', GenreViewSet, basename='genres')
 router.register(r'titles', TitleViewSet, basename='titles')
+
+urlpatterns = [
+    path("v1/",include(router.urls))
+]
